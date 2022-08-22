ea4b46d27696896c00eb2b800b9091f28f26ce0d
PabloAza89 <juanpabloazambuyo@gmail.com>
2022-08-22 00:34:01 -0300

    h

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
 
-export default function  Form() {
+/* export default function  Form() {
+  const [username, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  console.log(username)
   return (
+    <form>
       <div>
-        Componente Form
+        <label>Username:</label>
+        <input type="text" name="username" onChange={(e) => setUsername(e.target.value)} value={username}/>
       </div>
+      <div>
+        <label>Password:</label>
+        <input type="text" name="password" onChange={(e) => setPassword(e.target.value)} value={password}/>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+} */
+
+export default function  Form() {
+  const [input, setInput] = React.useState({
+    username: '',
+    password: '',
+  });
+
+  const handleInputChange = function(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Username:</label>
+        <input type="text" name="username" onChange={handleInputChange} value={username} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="text" name="password" onChange={handleInputChange} value={password} />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   )
 }
